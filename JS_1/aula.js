bce8780ee540fd90c747e583689495920f027124
Pedro Félix <pedrovitorsf@gmail.com>
2024-09-12 11:51:14 -0300

    feat: Data types

---
+++ JS_1/aula.js
@@ -0,0 +1,17 @@
+// TIPOS DE DADOS
+
+// Number: 1, 2, 3, 4 ...
+console.log(4);
+
+// BigInt: 99999999n ('n' sempre no final)
+console.log(999999999999n);
+
+// String: 'Pedro', "Pedro" ou `Pedro` (As strings podem ser interpretadas como listas "[]")
+console.log("Pedro"[0]); // Irá imprimir o 'P', caso utilize .lenght ele irá imprimir o seu tamanho.
+
+// Bolean: true ou false, verificação se o valor é verdadeiro ou falso
+console.log(true);
+
+// null: Se caracteriza como um valor vazio, porém existente.
+// undefined: Se caracteriza como um valor inexistente.
+// typeof: Expressão utilizada para retornar objetos.
