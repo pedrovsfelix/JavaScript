db83cae3fc7a19b8fb017191eff0e448cc768768
Pedro Félix <pedrovitorsf@gmail.com>
2024-09-18 14:27:28 -0300

    feat: arrays in javascript

---
+++ JS_2/array.js
@@ -0,0 +1,52 @@
+const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli"];
+console.log(arr);
+
+// Adicionando Elementos
+// push()
+let tamanho = arr.push("Boromir");
+console.log(arr);
+console.log(tamanho);
+
+// unshift
+tamanho = arr.unshift("Boromir");
+console.log(arr);
+console.log(tamanho);
+
+// Remover Elementos
+// pop()
+
+const ultimoElemento = arr.pop();
+console.log(arr);
+console.log(ultimoElemento);
+
+// Pesquisando Elementos
+// true
+const include = arr.includes("Gandalf");
+console.log(include);
+
+// posição
+const indice = arr.indexOf("Galdalf");
+console.log(indice);
+
+// Cortar e Concatenar
+// slice
+const hobbits = arr.slice(0, 4);
+console.log(hobbits);
+const outros = arr.slice(-4);
+console.log(outros);
+
+const socidade = hobbits.concat(outros, "Boromir");
+console.log(socidade);
+
+// Substituindo Elemento
+// splice
+const elementosRemovidos = socidade.splice(indice, 1, "Gandalf, o Cinzento");
+console.log(elementosRemovidos);
+console.log(socidade);
+
+// Iterando sobre os elementos
+// Usando o 'for' para percorrer cada elemento do array
+for(let indice = 0; indice < socidade.length; indice++){
+    const elemento = socidade[indice];
+    console.log(elemento + " se encontra na posição " + indice);
+}
---
+++ JS_2/aula.js
@@ -0,0 +1,14 @@
+// ESTRUTURAS DE DADOS E FUNÇÕES
+// ESTRUTURAS DE DADOS
+// Modos particular de armazenar e organizar dados de modo que possam ser utilizados eficientemente, facilitando
+// sua busca e modificação.
+
+// ESTRUTURA DE DADOS CLÁSSICA
+// LISTA LIGADA ->
+// ARRAY -> Coleção de ordenada de elementos
+
+// FILA (Queue) -> Primeiro a entrar, primeiro a sair.
+// push() e shift()
+
+// PILHA (Stack) -> Último a entrar, primeiro a sair
+// ÁRVORE (Tree) -> Conjunto de elementos chamados de "nós" (ou nodes), organizados em estrutura hierárquica (não sequencial).
