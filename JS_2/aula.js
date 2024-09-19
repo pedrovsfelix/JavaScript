4ad6ae65f09df2822153e8da3e41a17ac87c22f2
Pedro Félix <pedrovitorsf@gmail.com>
2024-09-19 08:34:34 -0300

    feat: objects

--- JS_2/aula.js
+++ JS_2/aula.js
@@ -12,3 +12,14 @@
 
 // PILHA (Stack) -> Último a entrar, primeiro a sair
 // ÁRVORE (Tree) -> Conjunto de elementos chamados de "nós" (ou nodes), organizados em estrutura hierárquica (não sequencial).
+
+// OBJETOS
+// Estrutura para trabalhar com dicionários de chave-valor
+// O par chave-valor em um objeto é chamado de propriedade
+// Diferente dos arrays, seus elementos não possuem nenhuma sequência.
+// As propriedades também podem ter strings, números e booleanos como chave.
+// Uma propriedade pode armazenar qualquer tipo de dado como valor.
+
+// let objeto = {}
+
+// As propriedades e funções podem ser referenciadas por encadeamento com ponto '.' ou colchetes '[]'
