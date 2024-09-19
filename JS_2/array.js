ee949af9bec571a879bd4f247197232e88ae7e98
Pedro Félix <pedrovitorsf@gmail.com>
2024-09-19 08:34:06 -0300

    feat: arrays bidimensionais

--- JS_2/array.js
+++ JS_2/array.js
@@ -1,4 +1,13 @@
-const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli"];
+const arr = [
+  "Frodo",
+  "Sam",
+  "Merry",
+  "Pippin",
+  "Gandalf",
+  "Aragorn",
+  "Legolas",
+  "Gimli",
+];
 console.log(arr);
 
 // Adicionando Elementos
@@ -20,7 +29,7 @@ console.log(arr);
 console.log(ultimoElemento);
 
 const primeiroElemento = arr.shift();
-console.log(primeiroElemento)
+console.log(primeiroElemento);
 
 // Pesquisando Elementos
 // true
@@ -49,7 +58,45 @@ console.log(socidade);
 
 // Iterando sobre os elementos
 // Usando o 'for' para percorrer cada elemento do array
-for(let indice = 0; indice < socidade.length; indice++){
-    const elemento = socidade[indice];
-    console.log(elemento + " se encontra na posição " + indice);
+for (let indice = 0; indice < socidade.length; indice++) {
+  const elemento = socidade[indice];
+  console.log(elemento + " se encontra na posição " + indice);
+}
+
+// Arrays bidimensionais
+const arrb = [
+  "1º Nível",
+  ["2º nível", 42, true],
+  [
+    ["3º nível, 1º item", "Olá, mundo!"],
+    ["3º nível, 2º item", "Oi, mundo!"],
+  ],
+  [],
+];
+
+// Arrays podem ter várias “dimensões”
+// Cada par de colchetes referencia um nível mais interno do array
+console.log(arrb[0]); // Saída: '1º Nível'
+console.log(arrb[1]); // Saída: [ '2º nível', 42, true ]
+console.log(arrb[1][0]); // Saída: '2º nível'
+console.log(arrb[1][1]); // Saída: 42
+console.log(arrb[2]); // Saída: [ [ '3º nível, 1º item', 'Olá, mundo!' ], [ '3º nível, 2º item', 'Oi, mundo!' ] ]
+console.log(arrb[2][0]); // Saída: [ '3º nível, 1º item', 'Olá, mundo!' ]
+console.log(arrb[2][1][1]); // Saída: 'Oi, mundo!
+
+// Podemos ver claramente a estrutura de uma matriz em uma tabela
+const matriz = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+  ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+  ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+];
+console.table(matriz);
+
+// Iterando sobre arrays bidimensionais (ou de qualquer dimensão)
+// Podemos usar vários níveis de laços FOR para iterar facilmente pela matriz
+for (let i = 0; i < matriz.length; i++) {
+  for (let j = 0; j < matriz[i].length; j++) {
+    const elemento = matriz[i][j];
+    console.log("Posição: (" + i + ", " + j + ") Valor: " + elemento);
+  }
 }
